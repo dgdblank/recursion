697f60711793498b12250749e72de4e9bbcfbfb2
Danielle Blank <dgdblank@gmail.com>
2015-04-24 13:43:06 -0700

    Added ability to stringify objects

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,12 +4,15 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
+  // stringify individual types of objects
   if( obj === undefined ){ return; }
   if( typeof obj === "function" ){ return; }
   if( typeof obj === "string" ){ return obj = '\"' + String(obj) + '\"';}
   if( typeof obj === "number" ){ return obj = String(obj);}
   if( typeof obj === "boolean" ){ return obj = String(obj);}
   if( obj === null ){ return obj = "null"; }
+  
+  // stringify arrays
   if( Array.isArray(obj)){
   	for(var i = 0; i < obj.length; i++){
   		obj[i] = stringifyJSON(obj[i]);
@@ -18,4 +21,15 @@ var stringifyJSON = function(obj) {
   	return '[' + obj + ']';
   }
 
+  // stringify objects
+  var propStrings = [];
+  if( typeof obj === "object" ){
+  	for(var prop in obj){
+  		var value = stringifyJSON(obj[prop]);
+  		prop = stringifyJSON(prop);
+  		propStrings.push(prop + ':' + value)
+  	}
+
+	return '{' + propStrings + '}'; 	
+  }
 };
