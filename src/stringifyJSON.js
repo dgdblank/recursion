d668f3724e57628e33b407d26f383812691d5b41
Danielle Blank <dgdblank@gmail.com>
2015-04-24 13:21:04 -0700

    Added ability to stringify arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,18 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if( obj === undefined ){ return; }
+  if( typeof obj === "function" ){ return; }
+  if( typeof obj === "string" ){ return obj = '\"' + String(obj) + '\"';}
+  if( typeof obj === "number" ){ return obj = String(obj);}
+  if( typeof obj === "boolean" ){ return obj = String(obj);}
+  if( obj === null ){ return obj = "null"; }
+  if( Array.isArray(obj)){
+  	for(var i = 0; i < obj.length; i++){
+  		obj[i] = stringifyJSON(obj[i]);
+  	}
+
+  	return '[' + obj + ']';
+  }
+
 };
